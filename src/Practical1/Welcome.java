33e874eb77dc4e4c86811519579345925d6c32e4
Wolfyre243 <wolfyreblueflare@gmail.com>
2025-04-22 09:25:11 +0800

    added test welcome class

---
+++ src/Practical1/Welcome.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Practical1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author wolfy
+ */
+public class Welcome {
+    public static void main(String args[]) {
+        JOptionPane.showMessageDialog(null, "Welcome to Java!", "Message", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
